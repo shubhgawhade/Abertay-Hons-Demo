7eb2f2db983ffddf9bf4759701c96b051c54220b
Shubh Gawhade <51043720+shubhgawhade@users.noreply.github.com>
2022-10-20 23:52:02 +0100

    Added player and scripts from test project

---
+++ Assets/Addons/Standard Assets 2018/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    
+    [RequireComponent(typeof (NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Vector3 target;                                    // target to aim for
+
+        private bool cooldown;
+        private RaycastHit hit;
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            // agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+        }
+
+
+        private void Update()
+        {
+            if (target == Vector3.zero)
+            {
+                target = transform.position;
+            }
+            
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0) && hit.collider.CompareTag("Ground"))
+            {
+                target = new Vector3(hit.point.x, 0, hit.point.z);
+                agent.SetDestination(target);
+            }
+            
+            if (agent.remainingDistance > 0.3f)
+            {
+                print(Mathf.Abs((transform.position - target).magnitude));
+                character.Move(agent.desiredVelocity, false, false);
+            }
+        }
+    }
+}
